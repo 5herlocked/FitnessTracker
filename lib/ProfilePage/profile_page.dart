8815d294b25529d6c4b25f900252dee948f88148
= <=>
2020-04-18 16:01:48 -0400

    Fixed indentations

--- lib/ProfilePage/profile_page.dart
+++ lib/ProfilePage/profile_page.dart
@@ -41,7 +41,8 @@ class _ProfilePageState extends State<ProfilePage> {
     final statusCode = 200;
     if (statusCode != 200) {
       setState(() {
-        errorMsg = "There has been an error processing your request. Please try again.";
+        errorMsg =
+            "There has been an error processing your request. Please try again.";
         _loading = false;
       });
       showDialog(
@@ -80,115 +81,117 @@ class _ProfilePageState extends State<ProfilePage> {
   // Build the Profile Form
   _buildForm() {
     return Form(
-      key: _formKey,
-      autovalidate: _autoValidate,
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
-            child: TextFormField(
-              onSaved: (input) => _description = input,
-              cursorColor: Decorations.accentColour,
-              validator: (input) =>
-              input.isEmpty ? "*Required" : null,
-              maxLines: 2,
-              decoration: Decorations.createInputDecoration(Icons.description, "Description"),
-              enabled: _isEnabled ? true : false,
-            ),
-          ),
-          Padding(
-              padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
+        key: _formKey,
+        autovalidate: _autoValidate,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
               child: TextFormField(
-                onSaved: (input) => _birthday = input,
+                onSaved: (input) => _description = input,
                 cursorColor: Decorations.accentColour,
                 validator: (input) => input.isEmpty ? "*Required" : null,
-                decoration: Decorations.createInputDecoration(Icons.calendar_today, "Birthday MM/DD/YYYY"),
+                maxLines: 2,
+                decoration: Decorations.createInputDecoration(
+                    Icons.description, "Description"),
                 enabled: _isEnabled ? true : false,
-              )
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              new Flexible(
-                  child: Container(
-                    width: 300,
-                    child: Padding(
-                        padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
-                        child: TextFormField(
-                          onSaved: (input) => _height = input,
-                          validator: (input) => input.isEmpty ? "*Required" : null,
-                          cursorColor: Decorations.accentColour,
-                          decoration: Decorations.createInputDecoration(null, "Height"),
-                          enabled: _isEnabled ? true : false,
-                        )
-                    ),
-                  )
               ),
-              new Flexible(
-                  child: Container(
-                    width: 300,
-                    child: Padding(
-                        padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
+            ),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
+                child: TextFormField(
+                  onSaved: (input) => _birthday = input,
+                  cursorColor: Decorations.accentColour,
+                  validator: (input) => input.isEmpty ? "*Required" : null,
+                  decoration: Decorations.createInputDecoration(
+                      Icons.calendar_today, "Birthday MM/DD/YYYY"),
+                  enabled: _isEnabled ? true : false,
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Flexible(
+                    child: Container(
+                      width: 300,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 10.0, left: 20, right: 20, bottom: 10),
+                          child: TextFormField(
+                            onSaved: (input) => _height = input,
+                            validator: (input) =>
+                                input.isEmpty ? "*Required" : null,
+                            cursorColor: Decorations.accentColour,
+                            decoration:
+                                Decorations.createInputDecoration(null, "Height"),
+                            enabled: _isEnabled ? true : false,
+                          )
+                      ),
+                )),
+                new Flexible(
+                    child: Container(
+                      width: 300,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: 10.0, left: 20, right: 20, bottom: 10),
                         child: TextFormField(
                           onSaved: (input) => _weight = input,
                           validator: (input) =>
-                          input.isEmpty ? "*Required" : null,
+                              input.isEmpty ? "*Required" : null,
                           cursorColor: Decorations.accentColour,
-                          decoration: Decorations.createInputDecoration(null, "Weight"),
+                          decoration:
+                              Decorations.createInputDecoration(null, "Weight"),
                           enabled: _isEnabled ? true : false,
                         )
-                    ),
-                  )
-              ),
-            ],
-          ),
-
-          Padding(
-              padding: EdgeInsets.only(bottom: 20, left: 20, right: 20),
-              child: TextFormField(
-                enabled: _isEnabled ? true : false,
-                onSaved: (input) => _fitnessGoal = input,
-                validator: (input) =>
-                input.isEmpty ? "*Required" : null,
-                cursorColor: Decorations.accentColour,
-                decoration: Decorations.createInputDecoration(Icons.fitness_center, "Fitness Goal"),
-              )
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: _loading == true
-                ? CircularProgressIndicator(
-              valueColor: new AlwaysStoppedAnimation<Color>(
-                  Decorations.accentColour),
-            )
-                : Container(
-              child: Visibility (
-                visible: _isSaveButtonVisible,
-                child: FlatButton(
-                  onPressed: () => _saveProfile(),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  splashColor: Colors.deepOrangeAccent,
-                  color: Decorations.accentColour,
-                  child: Text(
-                    "Save",
-                    style: Decorations.loginRegButton,
-                  ),
-                ),
-              ),
-              height: 55,
-              width: 150,
+                      ),
+                )),
+              ],
             ),
-          ),
-        ],
-      )
-    );
+            Padding(
+                padding: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                child: TextFormField(
+                  enabled: _isEnabled ? true : false,
+                  onSaved: (input) => _fitnessGoal = input,
+                  validator: (input) => input.isEmpty ? "*Required" : null,
+                  cursorColor: Decorations.accentColour,
+                  decoration: Decorations.createInputDecoration(
+                      Icons.fitness_center, "Fitness Goal"),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: _loading == true
+                  ? CircularProgressIndicator(
+                      valueColor: new AlwaysStoppedAnimation<Color>(
+                          Decorations.accentColour),
+                    )
+                  : Container(
+                      child: Visibility(
+                        visible: _isSaveButtonVisible,
+                        child: FlatButton(
+                          onPressed: () => _saveProfile(),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          splashColor: Colors.deepOrangeAccent,
+                          color: Decorations.accentColour,
+                          child: Text(
+                            "Save",
+                            style: Decorations.loginRegButton,
+                          ),
+                        ),
+                      ),
+                      height: 55,
+                      width: 150,
+                    ),
+            ),
+          ],
+        ));
   }
 
   @override
@@ -201,54 +204,56 @@ class _ProfilePageState extends State<ProfilePage> {
         appBar: AppBar(
           actions: <Widget>[],
           elevation: 0.0,
-          title: Text("Profile", style: TextStyle(fontFamily: 'Raleway'),),
+          title: Text(
+            "Profile",
+            style: TextStyle(fontFamily: 'Raleway'),
+          ),
         ),
         body: Container(
-          padding: EdgeInsets.only(top: 20),
-          child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 20,
-                        bottom: 10,
-                      ),
-                      child: CircleAvatar(
-                        backgroundImage: new NetworkImage(
-                            "https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/Profile-Pic-Circle-Grey-Large.png"),
-                        radius: 80.0,
-                      ),
+            padding: EdgeInsets.only(top: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      bottom: 10,
                     ),
-                    Visibility(
-                      visible: _isEnabled,
-                      child: Container(
-                        padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
-                        child: Text(
-                            "Choose to select a new photo.",
-                            style: Decorations.logIn
-                        ),
-                        alignment: Alignment.center,
-                      ),
+                    child: CircleAvatar(
+                      backgroundImage: new NetworkImage(
+                          "https://19yw4b240vb03ws8qm25h366-wpengine.netdna-ssl.com/wp-content/uploads/Profile-Pic-Circle-Grey-Large.png"),
+                      radius: 80.0,
                     ),
-                    Container(
-                      padding: EdgeInsets.only(top: 10.0, left: 20, right: 20, bottom: 10),
-                      child: Text(
-                        widget.client.firstName + "  " + widget.client.lastName,
-                        style: Decorations.profileUserName,
-                      ),
+                  ),
+                  Visibility(
+                    visible: _isEnabled,
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          top: 10.0, left: 20, right: 20, bottom: 10),
+                      child: Text("Choose to select a new photo.",
+                          style: Decorations.logIn),
                       alignment: Alignment.center,
                     ),
-                    SizedBox(
-                      height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                        top: 10.0, left: 20, right: 20, bottom: 10),
+                    child: Text(
+                      widget.client.firstName + "  " + widget.client.lastName,
+                      style: Decorations.profileUserName,
                     ),
-                    _buildForm()
-                  ],
-                ),
-              )
-          ),
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _buildForm()
+                ],
+              ),
+            )),
         floatingActionButton: new Visibility(
-          // if the text fields are enabled, then the floating action button
-          // should be invisible
+            // if the text fields are enabled, then the floating action button
+            // should be invisible
             visible: !_isEnabled,
             child: FloatingActionButton(
               onPressed: () {
